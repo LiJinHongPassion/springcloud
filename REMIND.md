227da1843276c5acbd836656fd2faa3d0989b6ec
李锦洪 <1755497577@qq.com>
2019-05-25 21:35:40 +0800

    6 SpringCloud Config分布式配置中心--- Config版的dept微服务

--- REMIND.md
+++ REMIND.md
@@ -110,13 +110,12 @@
             - 配置仓库新建文件microservicecloud-config-eureka-client.yml，并添加配置内容  **先要推送上github去**
             - springcloud-microservicecloud-config-3344 服务端
             - springcloud-microservicecloud-config-eureka-client-7001   
-        
-        
-        
-        
         - Config版的dept微服务
             - 配置仓库新建文件microservicecloud-config-dept-client.yml，并添加配置内容  **先要推送上github去**
-                        
+            - springcloud-microservicecloud-config-3344 服务端
+            - springcloud-microservicecloud-config-eureka-client-7001 
+            - springcloud-microservicecloud-config-provider-client-8081
+             
        
         当配置更改，需要重启config-3344配置中心，并且只要配置中心的yml文件更改后，需要重启对应微服务
         
---
+++ springcloud-microservicecloud-config-provider-client-8081/src/main/java/com/cqut/li/springcloud/controller/UserController_provider.java
@@ -0,0 +1,79 @@
+package com.cqut.li.springcloud.controller;
+
+
+import com.alibaba.fastjson.JSON;
+import com.cqut.li.springcloud.service.IUserService;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class UserController_provider {
+
+    //@Qualifier(value = "org.springframework.cloud.client.discovery.DiscoveryClient")
+    @Resource
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery()
+    {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
+    }
+
+
+    @Resource(name = "userService")
+    private IUserService userService;
+
+    @RequestMapping(value = "/user/login", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String login(@RequestParam("user_id") String user_id,
+                        @RequestParam("user_password") String user_password) {
+
+        List<Map<String, Object>> result = userService.login(user_id, user_password);
+
+        return JSON.toJSON(result).toString().replace("\"", "'");
+    }
+
+    @RequestMapping(value = "/user/register", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String register(@RequestParam("user_id") String user_id,
+                           @RequestParam("user_password") String user_password) {
+
+        Map<String, Object> result = userService.register(user_id, user_password);
+
+        return JSON.toJSON(result).toString().replace("\"", "'");
+    }
+
+    @RequestMapping(value = "/user/updateUser", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String updateUser(@RequestParam("properties") String properties) {
+
+        Map<String, Object> maps = (Map<String, Object>) JSON.parse(properties);
+
+        Map<String, Object> result = userService.updateUser(maps);
+
+        return JSON.toJSON(result).toString().replace("\"", "'");
+    }
+
+    @RequestMapping(value = "/user/getUserById", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String getUserById(@RequestParam("user_id") String user_id) {
+
+        Map<String, Object> result = userService.getUserById(user_id);
+        result.put("url", "springcloud-microservicecloud-eureka-provider-8081");
+
+        return JSON.toJSON(result).toString().replace("\"", "'");
+    }
+}
